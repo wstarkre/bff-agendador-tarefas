3e5b9509b9a1755067909fce4f1b2d2ac3d28506
Sabrina <sabrinavitreis@gmail.com>
2026-07-18 21:31:23 -0300

    Adicionado esqueleto do projeto

---
+++ src/main/java/com/javanauta/bffagendadortarefas/BffAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffagendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/bffagendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
