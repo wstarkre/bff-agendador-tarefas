2447d38ccab63059adcec90426655569c8aa5c72
Sabrina <sabrinavitreis@gmail.com>
2026-07-24 18:33:59 -0300

    adicionado client de email

---
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,13 @@
+package com.javanauta.bffagendadortarefas.infrastructure.client;
+
+import com.javanauta.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+
+    }
+
