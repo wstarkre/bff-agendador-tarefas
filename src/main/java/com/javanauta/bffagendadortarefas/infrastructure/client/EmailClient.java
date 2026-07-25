ace7e56bdba045fe782b0a58dfb44d10ab65b04a
Sabrina <sabrinavitreis@gmail.com>
2026-07-24 23:19:42 -0300

    adicionado Cron

--- src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 
     }
